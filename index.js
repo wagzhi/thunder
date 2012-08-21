6286d91673191b583ec211a37488f0868176a77c
Paul Wang <wagzhi@gmail.com>
2012-08-21 20:09:04 +0800

    init

---
+++ index.js
@@ -0,0 +1,83 @@
+//获取方法名称
+
+
+//参数是测试方法的数组
+function thunder(){
+	/**
+	* run a array of test method
+	*/
+	this.runTests=function(testMethods){
+		var tests=Object.keys(testMethods);
+		console.log("startting test ...");
+		console.log("total "+tests.length+" methods to run.");
+		
+		var i=0;	
+		//调用下一个测试方法
+		function testCaller(){
+			if(i<tests.length) {
+				console.log('-----------------------------------------------');//换行
+				console.log('calling test '+i+ ' '+ ': '+tests[i]);//(tests[i].toString()).match(/function\s*([^(]*)\(/)[1]+' ...');
+				testMethods[tests[i++]](testCaller);
+			}else{
+				console.log('-----------------------------------------------');
+				console.log('all test finished!');
+				return;
+			}
+		}
+	
+		testCaller();
+	}	
+	
+	/**
+	* find all test method name testXXX() , invoke mothod array of  [setup, testMethod1,...，testMethodn, clean]
+	*/
+	this.run=function(){
+		var methods=Object.getOwnPropertyNames(this);
+		var tests=new Array();
+		tests['setup']=this.setup;
+		var n=1;
+		for(var i=0;i<methods.length;i++){
+			if(methods[i].substring(0,4)=='test'){
+				tests[methods[i]]=this[methods[i]];
+			}
+		}
+		tests['clean']=this.clean;
+		this.runtest(tests);
+	}
+}
+thunder.prototype.setup=function(callback){
+	callback();
+};
+thunder.prototype.clean=function(){
+};
+
+
+thunder.prototype.assert=function(flag,message){
+	if(!flag){
+		console.log("Assert faild: "+message);
+		process.exit();
+	}
+}
+
+
+function assertEquals(arg1,arg2,errorMessage){
+	if(arg1===arg2){
+		//passed
+	}
+	else{
+		console.log( "Test failed, expect "+arg1+" but "+arg2);
+		console.log("Message: "+errorMessage);
+	}
+	process.exit();
+}
+
+function assert(result,errorMessage){
+	console.log( "Test failed!")
+	console.log("Message: "+errorMessage);
+	process.exit();
+}
+
+exports.thunder=thunder;
+exports.assert=assert;
+exports.assertEquals=assertEquals;
+
